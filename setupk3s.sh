4cb60ce0acbab916d95edf22b83225f004e711d2
Danny De Novi <dnvdny00m29f158s@studenti.unime.it>
2022-08-07 16:25:37 +0200

    aggiunta installazione iptables

--- setupk3s.sh
+++ setupk3s.sh
@@ -1,3 +1,4 @@
+sudo apt install iptables -y
 sudo iptables -F
 sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
 sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
