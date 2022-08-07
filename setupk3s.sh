ced6456053e16d2520f4d0c7eab3d1653acc7d80
Danny De Novi <dnvdny00m29f158s@studenti.unime.it>
2022-08-07 16:12:08 +0200

    Creato setup per k3s

---
+++ setupk3s.sh
@@ -0,0 +1,5 @@
+sudo iptables -F
+sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+sudo echo cgroup_memory=1 cgroup_enable=memory >> /boot/cmdline.txt
+sudo reboot
