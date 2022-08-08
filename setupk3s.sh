6dac8a8aef56e4ac81baba353ca808374ed54567
Danny De Novi <70696078+dannydenovi@users.noreply.github.com>
2022-08-08 12:12:32 +0200

    Update setupk3s.sh

--- setupk3s.sh
+++ setupk3s.sh
@@ -2,5 +2,5 @@ sudo apt install iptables -y
 sudo iptables -F
 sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
 sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
-sudo echo cgroup_memory=1 cgroup_enable=memory >> /boot/cmdline.txt
+sudo echo " cgroup_memory=1 cgroup_enable=memory" >> /boot/cmdline.txt
 sudo reboot
